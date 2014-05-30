fe2801fb493e62792bcef25241be6d0f745431a7
ankitag984 <ankit.ag.in@gmail.com>
2014-05-31 01:16:34 +0530

    REST Service added
    REST Service added

---
+++ IntuitRS/src/com/ankit/domain/PersonChild.java
@@ -0,0 +1,19 @@
+package com.ankit.domain;
+
+public class PersonChild extends Person {
+	
+	public static void main(String[] args){
+		PersonChild child = new PersonChild();
+		child.testmeChild();
+		byte a = 3;
+		byte b = 2;
+		//byte c = a + b ;
+		
+	}
+	
+	public void testmeChild(){
+		
+	}
+	
+	
+}
